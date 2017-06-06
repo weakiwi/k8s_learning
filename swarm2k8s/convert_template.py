794c5d18ca8778e2f3bbf91fe1f363ca15fa6e9d
root <root@localhost.localdomain>
2017-06-06 19:50:15 +0800

    fix bugs

--- swarm2k8s/convert_template.py
+++ swarm2k8s/convert_template.py
@@ -1,9 +1,25 @@
 from flask import Flask,render_template
 import yaml
+import re
 
 app = Flask(__name__)
 app.jinja_env.trim_blocks = True  
 app.jinja_env.lstrip_blocks = True  
+def del_bracket(l):
+	return l[0]
+def env_deal(l):
+	if type(l) == list:
+		return l.split("=",1)[0]
+	if type(l) == dict:
+		return l.keys()
+def env_deal_1(l):
+	if type(l) == list:
+		return l.split("=",1)[1]
+	if type(l) == dict:
+		return l.values()
+app.add_template_filter(del_bracket, 'del_bracket')
+app.add_template_filter(env_deal, 'env_deal')
+app.add_template_filter(env_deal_1, 'env_deal_1')
  
 @app.route('/hello')
 @app.route('/hello/<name>')
@@ -12,7 +28,7 @@ def hello(name=None):
 	dataMap = yaml.load(f)
 	f.close()
 	services = dataMap['services']
-	ports=[value["ports"] for value in dataMap["services"].values()]
+	ports=[value["ports"] if "ports" in value else None for value in dataMap["services"].values()]
 	images=[value["image"] for value in dataMap["services"].values()]
 #	if "commands" in services:
 #		commands=[value["command"] for value in dataMap["services"].values()]
@@ -24,9 +40,19 @@ def hello(name=None):
 			commands.append(i["command"])
 		else:
 			commands.append(None)
-	environment=[value["environment"] for value in dataMap["services"].values() if "environment" in value]  if "services" in dataMap else []
+	environment = [value["environment"] for value in dataMap["services"].values() if "environment" in value]  if "services" in dataMap else []
+	tmp_env = []
+	tmp_dict = {}
+	for i in environment:
+		if type(i) == dict:
+			tmp_env.append(i)
+		elif type(i) == list:
+			for j in i:
+				tmp_dict[j.split("=",1)[0]]=j.split("=",1)[1]
+			tmp_env.append(tmp_dict)
+			tmp_dict = {}
 	labels=[map(lambda x:x.split('=', 1) ,map(lambda x:x.replace('"',''), value["labels"])) for value in dataMap["services"].values() if "labels" in value]  if "services" in dataMap else []
-	mountpaths=[value["volumes"] if "volumes" in value else [] for value in dataMap["services"].values()]  if "services" in dataMap else []
+	mountpaths=[value["volumes"] if "volumes" in value else None for value in dataMap["services"].values()]  if "services" in dataMap else []
 	volumes = []
 	storages = []
 	counters = 0
@@ -37,8 +63,10 @@ def hello(name=None):
 			counters = counters + 1
 		else:
 			volumes.append(None)
+			storages.append(None)
+			counters = counters + 1
 	namespace = "tmpns"
-	return render_template('hello.html', namespace=namespace, services=zip(services, ports), volumes=zip(volumes, storages), statefulsets=zip(services,images,commands,environment,labels,zip(volumes,mountpaths)))
+	return render_template('hello.html', namespace=namespace, services=zip(services, ports), volumes=zip(volumes, storages), statefulsets=zip(services,images,commands,tmp_env,labels,zip(volumes,mountpaths)))
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
