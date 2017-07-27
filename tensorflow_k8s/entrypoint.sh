afa948f2007be8359a6e4e2788b4e2d1c29faa0c
weakiwi <670932228@qq.com>
2017-07-27 18:58:20 +0800

    modify

--- tensorflow_k8s/entrypoint.sh
+++ tensorflow_k8s/entrypoint.sh
@@ -15,8 +15,8 @@ check_env $WORK_HOST
 export TASK_INDEX=$(hostname| grep -o '[0-9]')
 export JOB_NAME=$(hostname| grep -o -e "worker" -e "ps")
 if [ $ADDRESS ];
-then
-#/ping.py
-fi
+#then
+##/ping.py
+#fi
 cd $(dirname $WORKDIR)
 python $(basename $WORKDIR) --ps_hosts=$PS_HOST --worker_hosts=$WORK_HOST --job_name=$JOB_NAME --task_index=$TASK_INDEX
