eb251a2243c76ea3a6b821ad86234d62d2376a52
weakiwi <670932228@qq.com>
2017-07-27 21:29:48 +0800

    modify

--- tensorflow_k8s/entrypoint.sh
+++ tensorflow_k8s/entrypoint.sh
@@ -21,16 +21,5 @@ export JOB_NAME=$(hostname| grep -o -e "worker" -e "ps")
 #cd $(dirname $WORKDIR)
 #sleep 10
 #cat /data/hosts >> /etc/hosts
-tmp_str="ping -c1 "
-tmp_str_1=$(echo $ADDRESS| sed 's/\,/\&\& ping -c1 /g')
-new_str=${tmp_str}${tmp_str_1}
-while /bin/true
-do
-        echo $new_str
-        eval $new_str
-        if [ $? -eq 0 ];
-        then
-                break
-        fi
-done
+/ipcheck
 python $(basename $WORKDIR) --ps_hosts=$PS_HOST --worker_hosts=$WORK_HOST --job_name=$JOB_NAME --task_index=$TASK_INDEX
