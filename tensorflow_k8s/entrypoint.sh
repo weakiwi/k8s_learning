07aca51399c4199afc38e0d574962381f194ed35
weakiwi <670932228@qq.com>
2017-07-27 16:00:24 +0800

    modify

--- tensorflow_k8s/entrypoint.sh
+++ tensorflow_k8s/entrypoint.sh
@@ -11,9 +11,11 @@ function check_env() {
 check_env $WORKDIR
 check_env $PS_HOST
 check_env $WORK_HOST
-check_env $ADDRESS
 
 export TASK_INDEX=$(hostname| grep -o '[0-9]')
 export JOB_NAME=$(hostname| grep -o -e "worker" -e "ps")
+if [ $ADDRESS ];
+then
 /portcheck
+fi
 python $WORKDIR --ps_hosts=$PS_HOST --worker_hosts=$WORK_HOST --job_name=$JOB_NAME --task_index=$TASK_INDEX
