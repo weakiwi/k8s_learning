c9f796b879153e667d96b06b8a043dd972f1b5fc
weakiwi <670932228@qq.com>
2017-07-28 14:20:39 +0800

    modify

--- tensorflow_k8s/entrypoint.sh
+++ tensorflow_k8s/entrypoint.sh
@@ -9,23 +9,23 @@ function check_env() {
     fi
 }
 
-check_env $WORKDIR
-check_env $PS_HOST
-check_env $WORK_HOST
-
-
-#get environemnt from hostname.Please follow the name rules to make it works
-export TASK_INDEX=$(hostname| grep -o '[0-9]')
-export JOB_NAME=$(hostname| grep -o -e "worker" -e "ps")
-cd $(dirname $WORKDIR)
-
-
-#start check if name server start working
-/ipcheck
-
-#tensorflow start
 if [ -z $CHECKPOINT_DIR ];
-then
+    then
+    check_env $WORKDIR
+    check_env $PS_HOST
+    check_env $WORK_HOST
+    
+    
+    #get environemnt from hostname.Please follow the name rules to make it works
+    export TASK_INDEX=$(hostname| grep -o '[0-9]')
+    export JOB_NAME=$(hostname| grep -o -e "worker" -e "ps")
+    cd $(dirname $WORKDIR)
+    
+    
+    #start check if name server start working
+    /ipcheck
+    
+    #tensorflow start
     python $(basename $WORKDIR) --ps_hosts=$PS_HOST --worker_hosts=$WORK_HOST --job_name=$JOB_NAME --task_index=$TASK_INDEX
 else
     python /usr/local/bin/tensorboard --logdir $CHECKPOINT_DIR --host 0.0.0.0
