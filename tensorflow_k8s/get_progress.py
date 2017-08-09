82f8a8b04eea24edc551c9557f7e0547c87d25e5
weakiwi <670932228@qq.com>
2017-08-09 15:26:33 +0800

    modify

---
+++ tensorflow_k8s/get_progress.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import math
+import os.path
+import os
+
+
+import numpy as np
+train_data_pos = os.environ.get("TRAIN_DATA_POS")
+test_data_pos = os.environ.get("TEST_DATA_POS")
+checkpoint_data_pos = os.environ.get("CHECKPOINT_DATA_POS")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/getprogress/')
+def getprogress():
+	batch_size = request.args.get("batch_size")
+	epoch_num = request.args.get("epoch_num")
+	validate_interval = request.args.get("validate_interval")
+	ckpt = tf.train.get_checkpoint_state(checkpoint_data_pos)
+	global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+	train_data_row = sum(1 for _ in tf.python_io.tf_record_iterator(train_data_pos))
+	test_data_row = sum(1 for _ in tf.python_io.tf_record_iterator(test_data_pos))
+	full_step = train_data_row*epoch_num/batch_size
+	validate_step = full_step/validate_interval
+	return global_step/(full_step - validate_step)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
