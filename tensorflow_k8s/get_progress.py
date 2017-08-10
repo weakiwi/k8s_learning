eb911a5b944fd15c6440b86e5c0c0c8e386f78ff
weakiwi <670932228@qq.com>
2017-08-10 13:23:46 +0800

    modify

--- tensorflow_k8s/get_progress.py
+++ tensorflow_k8s/get_progress.py
@@ -22,14 +22,17 @@ app = Flask(__name__)
 def getprogress():
 	batch_size = request.args.get("batch_size")
 	epoch_num = request.args.get("epoch_num")
-	validate_interval = request.args.get("validate_interval")
-	ckpt = tf.train.get_checkpoint_state(checkpoint_data_pos)
-	global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
-	train_data_row = sum(1 for _ in tf.python_io.tf_record_iterator(train_data_pos))
-	test_data_row = sum(1 for _ in tf.python_io.tf_record_iterator(test_data_pos))
-	full_step = train_data_row*epoch_num/batch_size
-	validate_step = full_step/validate_interval
-	return global_step/(full_step - validate_step)
+        if os.path.isfile(train_data_pos) and os.path.isfile(test_data_pos) and os.path.isfile(checkpoint_data_pos):
+	    validate_interval = request.args.get("validate_interval")
+	    ckpt = tf.train.get_checkpoint_state(checkpoint_data_pos)
+	    global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+	    train_data_row = sum(1 for _ in tf.python_io.tf_record_iterator(train_data_pos))
+	    test_data_row = sum(1 for _ in tf.python_io.tf_record_iterator(test_data_pos))
+	    full_step = train_data_row*epoch_num/batch_size
+	    validate_step = full_step/validate_interval
+	    return global_step/(full_step - validate_step)
+        else:
+            return "not file exists"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
 
